756e136ccc5670749f07db38e51b0982c159eadb
rugii913 <rugii913@gmail.com>
2023-08-30 18:13:41 +0900

    p1 테스트 코드 실습-비... @ParameterizedTest, @ValueSource
    ㅁ@ParameterizedTest, @ValueSource를 활용한 경계값 테스트

--- part1/mvcPractice/src/test/java/org/example/PasswordValidatorTest.java
+++ part1/mvcPractice/src/test/java/org/example/PasswordValidatorTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -19,10 +21,11 @@ public class PasswordValidatorTest {
                 .doesNotThrowAnyException();
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
-    void validatePasswordTest2() {
-        assertThatCode(() -> PasswordValidator.validate("aabb"))
+    @ValueSource(strings = {"aabbcce", "aabbccddeeffg"}) // 7자, 13자 경계값
+    void validatePasswordTest2(String password) {
+        assertThatCode(() -> PasswordValidator.validate(password))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(PasswordValidator.WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAGE);
     }
