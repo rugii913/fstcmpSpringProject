0af2053923f87b67c1151a4527d8c7093b5143f8
rugii913 <rugii913@gmail.com>
2023-08-30 19:05:03 +0900

    p1 테..-비..-테스트 컨트롤을 위한 인터페이스(낮은 결합도)
    ㅁ테스트 컨트롤을 위해 인터페이스를 두고 User의 initPassword(~)에서 주입받는 방식으로 변경
     - 기존에는 initPassword(~) 안에서 RandomPasswordGenerator를 생성했음
     -> 테스트를 쉽게 만들기 위해 RandomPasswordGenerator 의존성 제거
      - 인터페이스 PasswordGenerator 타입을 주입받는 방식으로 변경
     => 테스트하기 쉬운 코드를 작성하다 보면 더 낮은 결합도를 가진 설계를 얻을 수도 있다.

--- part1/mvcPractice/src/main/java/org/example/User.java
+++ part1/mvcPractice/src/main/java/org/example/User.java
@@ -7,14 +7,19 @@ public class User {
         return password;
     }
 
-    public void initPassword() {
-        RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
-        String randomPassword = randomPasswordGenerator.generatePassword();
+    public void initPassword(PasswordGenerator passwordGenerator) {
+        // as-is 방식: 내부에서 생성 -> 강한 결합도
+        // RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
+        // String password = randomPasswordGenerator.generatePassword();
+
+        // to-be 방식: 인터페이스를 두고 주입받음 -> 낮은 결합도
+        String password = passwordGenerator.generatePassword();
+
         /*
          * 비밀번호는 8자 이상 12자 이하여야 한다.
          */
-        if (randomPassword.length() >= 8 && randomPassword.length() <= 12) {
-            this.password = randomPassword;
+        if (password.length() >= 8 && password.length() <= 12) {
+            this.password = password;
         }
     }
 }
--- part1/mvcPractice/src/test/java/org/example/UserTest.java
+++ part1/mvcPractice/src/test/java/org/example/UserTest.java
@@ -8,14 +8,27 @@ class UserTest {
 
     @Test
     @DisplayName("패스워드를 초기화한다.")
-    void passwordTest() {
+    void passwordTest1() {
         // given
         User user = new User();
 
         // when
-        user.initPassword();
+        user.initPassword(new CorrectFixedPasswordGenerator());
 
         // then
         Assertions.assertThat(user.getPassword()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("패스워드가 요구사항에 부합되지 않아 초기화가 되지 않는다.")
+    void passwordTest2() {
+        // given
+        User user = new User();
+
+        // when
+        user.initPassword(new WrongFixedPasswordGenerator());
+
+        // then
+        Assertions.assertThat(user.getPassword()).isNull();
     }
 }
