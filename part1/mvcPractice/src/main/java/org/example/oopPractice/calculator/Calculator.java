9e2c2e97a298214ed1f506b609830ca3ed834531
rugii913 <rugii913@gmail.com>
2023-08-31 01:46:25 +0900

    p1 사칙..-덧셈 부분 리팩토링
    * 테스트 코드가 있으면 리팩토링 후에 바로 검사할 수 있기 때문에 심리적인 안정감을 얻을 수 있다.

--- part1/mvcPractice/src/main/java/org/example/oopPractice/calculator/Calculator.java
+++ part1/mvcPractice/src/main/java/org/example/oopPractice/calculator/Calculator.java
@@ -3,6 +3,9 @@ package org.example.oopPractice.calculator;
 public class Calculator {
 
     public static int calculate(int operand1, String operator, int operand2) {
-        return operand1 + operand2;
+        if ("+".equals(operator)) {
+            return operand1 + operand2;
+        }
+        return 0;
     }
 }
