0f412907a58a0662a59e1c78478e92b774e6b6e7
rugii913 <rugii913@gmail.com>
2023-08-31 01:42:26 +0900

    p1 사칙..-덧셈-테스트가 통과만 하는 코드 *파라미터를 활용하지 않음
    *파라미터 operator를 사용하지 않았음
    *일단 테스트를 통과시켜서 심리적 안정감을 얻기

--- part1/mvcPractice/src/main/java/org/example/oopPractice/calculator/Calculator.java
+++ part1/mvcPractice/src/main/java/org/example/oopPractice/calculator/Calculator.java
@@ -3,6 +3,6 @@ package org.example.oopPractice.calculator;
 public class Calculator {
 
     public static int calculate(int operand1, String operator, int operand2) {
-
+        return operand1 + operand2;
     }
 }
