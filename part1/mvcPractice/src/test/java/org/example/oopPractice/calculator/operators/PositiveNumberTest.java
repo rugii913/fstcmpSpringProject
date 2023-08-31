9df47eb9a4330369af20a99264e373478f1cba82
rugii913 <rugii913@gmail.com>
2023-08-31 11:24:04 +0900

    p1 사칙..-PositiveNumber @ParameterizedTest 및 정리
    ㅁ@ParameterizedTest 사용하여 생성자의 다양한 arg에 대한 테스트
    
    *** 정리
     - 클라이언트에서 Calculator의 calculate 메서드(인터페이스)에 작업 위임
     - 그러면 Calculator는 arithmeticOperators 객체들에 위임
     - 이러한 객체들의 호출을 메시지를 전달한다고 표현함
     - 객체지향적인 프로그래밍이라 할 수 있음

--- part1/mvcPractice/src/test/java/org/example/oopPractice/calculator/operators/PositiveNumberTest.java
+++ part1/mvcPractice/src/test/java/org/example/oopPractice/calculator/operators/PositiveNumberTest.java
@@ -1,14 +1,16 @@
 package org.example.oopPractice.calculator.operators;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class PositiveNumberTest {
 
-    @Test
-    void createTest() {
-        assertThatCode(() -> new PositiveNumber(0))
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1})
+    void createTest(int value) {
+        assertThatCode(() -> new PositiveNumber(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("0 또는 음수를 전달할 수 없습니다.");
     }
