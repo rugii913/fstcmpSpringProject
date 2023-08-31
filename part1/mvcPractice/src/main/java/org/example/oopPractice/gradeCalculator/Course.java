da4b30d1245ea1acf026dcd6a589e829f8cc48de
rugii913 <rugii913@gmail.com>
2023-08-31 11:56:17 +0900

    p1 학점..-테스트에 맞춰 Course 필드 및 생성자 작성, 테스트 성공

---
+++ part1/mvcPractice/src/main/java/org/example/oopPractice/gradeCalculator/Course.java
@@ -0,0 +1,14 @@
+package org.example.oopPractice.gradeCalculator;
+
+public class Course {
+
+    private final String subject; // 과목명
+    private final int credit; // 학점
+    private final String grade; // 성적
+
+    public Course(String subject, int credit, String grade) {
+        this.subject = subject;
+        this.credit = credit;
+        this.grade = grade;
+    }
+}
