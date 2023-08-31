c6b0b2bc61ac2dd105225a781120e6fb55ca0bbb
rugii913 <rugii913@gmail.com>
2023-08-31 10:16:02 +0900

    p1 사칙..-enum 사용 리팩토링
    ㅁCalculator - ArithmeticOperator의 static calculate(~) 사용
    ㅁArithmeticOperator
     - static calculate(~) 메서드는 해당되는 객체를 찾아내고
     - 각 객체에서 재정의된 arithmeticCalculate(~)를 실행

--- part1/mvcPractice/src/main/java/org/example/oopPractice/calculator/ArithmeticOperator.java
+++ part1/mvcPractice/src/main/java/org/example/oopPractice/calculator/ArithmeticOperator.java
@@ -1,25 +1,27 @@
 package org.example.oopPractice.calculator;
 
+import java.util.Arrays;
+
 public enum ArithmeticOperator {
     ADDITION("+") {
         @Override
-        public int calculate(int operand1, int operand2) {
+        public int arithmeticCalculate(int operand1, int operand2) {
             return operand1 + operand2;
         }
     }, SUBTRACTION("-") {
         @Override
-        public int calculate(int operand1, int operand2) {
+        public int arithmeticCalculate(int operand1, int operand2) {
             return operand1 - operand2;
         }
     }, MULTIPLICATION("*") {
         @Override
-        public int calculate(int operand1, int operand2) {
+        public int arithmeticCalculate(int operand1, int operand2) {
             return operand1 * operand2;
         }
     }, DIVISION("/") {
         @Override
-        public int calculate(int operand1, int operand2) {
-            return operand1 * operand2;
+        public int arithmeticCalculate(int operand1, int operand2) {
+            return operand1 / operand2;
         }
     };
 
@@ -29,5 +31,15 @@ public enum ArithmeticOperator {
         this.operator = operator;
     }
 
-    public abstract int calculate(final int operand1, final int operand2);
+    public abstract int arithmeticCalculate(final int operand1, final int operand2);
+
+    public static int calculate(int operand1, String operator, int operand2) {
+        ArithmeticOperator arithmeticOperator = Arrays.stream(values()) // values(): 모든 enum 객체들을 가져옴
+                .filter(v -> v.operator.equals(operator)) // array로 가져온 객체들의 operator가 파라미터 operator와 같은 것 필터
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
+
+        // 찾아낸 enum 타입 객체가 가진 arithmeticCalculate(~) 메서드를 실행
+        return arithmeticOperator.arithmeticCalculate(operand1, operand2);
+    }
 }
