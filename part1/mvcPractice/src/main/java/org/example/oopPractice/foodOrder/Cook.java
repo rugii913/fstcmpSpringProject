dc11571a62893d85073fe545547fc3a947d776fe
rugii913 <rugii913@gmail.com>
2023-09-02 00:02:33 +0900

    p1 음식..-Cook 간이 작성 및 생성 테스트 통과

---
+++ part1/mvcPractice/src/main/java/org/example/oopPractice/foodOrder/Cook.java
@@ -0,0 +1,11 @@
+package org.example.oopPractice.foodOrder;
+
+public class Cook {
+    private final String name;
+    private final int price;
+
+    public Cook(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
