c0bf9249c71412ef4924849923bdb2e81de20881
rugii913 <rugii913@gmail.com>
2023-09-02 01:08:15 +0900

    p1 음식..-Customer 선테스트 작성

---
+++ part1/mvcPractice/src/main/java/org/example/oopPractice/foodOrder/Customer.java
@@ -0,0 +1,8 @@
+package org.example.oopPractice.foodOrder;
+
+public class Customer {
+
+    public void order(String menuName, Menu menu, Chef chef) {
+
+    }
+}
--- part1/mvcPractice/src/test/java/org/example/oopPractice/foodOrder/CustomerTest.java
+++ part1/mvcPractice/src/test/java/org/example/oopPractice/foodOrder/CustomerTest.java
@@ -1,12 +1,22 @@
 package org.example.oopPractice.foodOrder;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class CustomerTest {
     
     @Test
-    void name() {
+    @DisplayName("메뉴 이름에 해당하는 요리를 주문한다.")
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        Chef chef = new Chef();
 
+        assertThatCode(() -> customer.order("만두", menu, chef)).doesNotThrowAnyException();
     }
 }
 /*
