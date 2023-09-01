8ce4cd5f6fc5dbcda1ec5d44fbcce1a2581e63f2
rugii913 <rugii913@gmail.com>
2023-09-02 00:19:06 +0900

    p1 음식..-ChefTest 통과 *eqauls(), hashCode() 재정의

--- part1/mvcPractice/src/main/java/org/example/oopPractice/foodOrder/Cook.java
+++ part1/mvcPractice/src/main/java/org/example/oopPractice/foodOrder/Cook.java
@@ -1,5 +1,7 @@
 package org.example.oopPractice.foodOrder;
 
+import java.util.Objects;
+
 public class Cook {
     private final String name;
     private final int price;
@@ -7,5 +9,18 @@ public class Cook {
     public Cook(String name, int price) {
         this.name = name;
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cook cook = (Cook) o;
+        return price == cook.price && Objects.equals(name, cook.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
