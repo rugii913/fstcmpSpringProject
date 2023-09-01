4ae3e81c04f0b0a22f751395ae7f3f21758b095d
rugii913 <rugii913@gmail.com>
2023-09-02 03:11:29 +0900

    p1 계웹-CustomWebApplicationServer 생성자, start() 메서드
    ㅁCustomWebApplicationServer
     ㅇport 번호 넣어서, 필드 port 갖도록 생성
     ㅇstart() 메서드 - ServerSocket try with resources
      - 서버소켓 객체 생성하여 자원으로 갖고 있기
      - 소켓 clientSocket 변수 선언 - 클라 기다리기
      - 서버소켓 객체가 accept() 했을 때 반환되는 소켓을 위 clientSocket 변수에 담고
      - 이게 null이 아니라면 로그 남기기
    
    ㅁMain에서 main 메서드로 CustomWebApplicationServer 시작

--- part1/mvcPractice/src/main/java/org/example/wasPractice/CustomWebApplicationServer.java
+++ part1/mvcPractice/src/main/java/org/example/wasPractice/CustomWebApplicationServer.java
@@ -1,8 +1,31 @@
 package org.example.wasPractice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class CustomWebApplicationServer {
 
-    public void start() {
-        
+    private final int port;
+    public static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
+
+    public CustomWebApplicationServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            logger.info("[CustomWebApplicationServer] started {} port.", port);
+
+            Socket clientSocket;
+            logger.info("[CustomWebApplicationServer] waiting for client");
+
+            while ((clientSocket = serverSocket.accept()) != null) {
+                logger.info("[CustomWebApplicationServer] client connected!");
+            }
+        }
     }
 }
