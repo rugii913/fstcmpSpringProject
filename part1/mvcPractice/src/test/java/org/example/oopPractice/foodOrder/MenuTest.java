2ecad9aee6aae34ce5a507536c9851382b8d1c73
rugii913 <rugii913@gmail.com>
2023-09-02 00:40:12 +0900

    p1 음식..-menu.choose(~) 네거티브 테스트

--- part1/mvcPractice/src/test/java/org/example/oopPractice/foodOrder/MenuTest.java
+++ part1/mvcPractice/src/test/java/org/example/oopPractice/foodOrder/MenuTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 public class MenuTest {
 
@@ -16,5 +17,15 @@ public class MenuTest {
 
         MenuItem menuItem = menu.choose("돈까스");
         assertThat(menuItem).isEqualTo(new MenuItem("돈까스", 5000));
+    }
+
+    @Test
+    @DisplayName("메뉴판에 없는 메뉴를 선택할 시 예외를 반환한다.")
+    void chooseTest2() {
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 7000)));
+        
+        assertThatCode(() -> menu.choose("통닭"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 메뉴 이름입니다.");
     }
 }
