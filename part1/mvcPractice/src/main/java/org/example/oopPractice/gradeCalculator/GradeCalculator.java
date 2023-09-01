bab9af8ffebe7785aaf737dcba90732f1aeed761
rugii913 <rugii913@gmail.com>
2023-09-01 23:49:08 +0900

    p1 학점..-주석 추가(핵심 포인트 - 협력 설계)

--- part1/mvcPractice/src/main/java/org/example/oopPractice/gradeCalculator/GradeCalculator.java
+++ part1/mvcPractice/src/main/java/org/example/oopPractice/gradeCalculator/GradeCalculator.java
@@ -10,6 +10,12 @@ public class GradeCalculator {
         this.courses = new Courses(courses);
     }
 
+    /**
+     * 핵심 포인트 - 협력을 설계
+     */
+    // 이수한 과목을 전달하여 평균학점 계산 요청 --> 학점 계산기 --> (학점 수 * 교과목 평점)의 합계 --> 과목(코스) 일급 컬렉션
+    //                                                  --> 수강신청 총 학점 수           --> 과목(코스) 일급 컬렉션
+    // 계산기가 직접 작업할 수도 있겠지만 요청하는 것으로 구상: 왜? 과목에는 해당 자료구조에 대한 정보(학점 수, 평점 등)를 갖고 있을 것이기 때문
     public double calculateGrade() {
         // (학점 수 * 교과목 평점)의 합계
         double totalMultipliedCreditAndCourseGrade = courses.multiplyCreditAndCourseGrade();
