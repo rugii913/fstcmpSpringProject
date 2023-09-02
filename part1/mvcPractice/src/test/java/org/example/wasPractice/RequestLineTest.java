1d8c0854c44305b63f6f91249fb6698bd5858ce4
rugii913 <rugii913@gmail.com>
2023-09-02 12:59:27 +0900

    p1 계웹-HttpRequest 객체 생각, RequestLine 선테스트 준비 중

---
+++ part1/mvcPractice/src/test/java/org/example/wasPractice/RequestLineTest.java
@@ -0,0 +1,13 @@
+package org.example.wasPractice;
+
+/*
+ * HttpRequest 객체
+ * * http 메시지를 readLine()으로 읽고 나서, HttpRequest라는 객체로 만든다면, 그 내부에는 무엇이 있을까?
+ * - requestLine
+ * - header
+ * - body
+ * * 위 중에서 계산기 어플에는 requestLine만 필요할 것
+ */
+public class RequestLineTest {
+    
+}
