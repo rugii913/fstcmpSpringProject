7da7290f58e8b018b28a815e7d16531207dfe51d
rugii913 <rugii913@gmail.com>
2023-09-03 00:03:58 +0900

    p1 계웹-RequestLine 클래스 작성(split() 이용)

--- part1/mvcPractice/src/main/java/org/example/wasPractice/Main.java
+++ part1/mvcPractice/src/main/java/org/example/wasPractice/Main.java
@@ -2,7 +2,7 @@ package org.example.wasPractice;
 
 import java.io.IOException;
 
-// GET /calculate?operand1=11&operator=*&operand2=55
+// GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
 public class Main {
     public static void main(String[] args) throws IOException {
         new CustomWebApplicationServer(8080).start();
--- part1/mvcPractice/src/test/java/org/example/wasPractice/RequestLineTest.java
+++ part1/mvcPractice/src/test/java/org/example/wasPractice/RequestLineTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.*;
 /*
  * HttpRequest 객체
  * * http 메시지를 readLine()으로 읽고 나서, HttpRequest라는 객체로 만든다면, 그 내부에는 무엇이 있을까?
- * - requestLine (ex. GET http://localhost:8080/calculate?operand1=11&operator=*&operand2=55 HTTP/1.1)
+ * - requestLine (ex. GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1)
  *  - httpMethod
  *  - path
  *  - queryString
@@ -21,7 +21,8 @@ public class RequestLineTest {
 
     @Test
     void create() {
-        RequestLine requestLine = new RequestLine("GET http://localhost:8080/calculate?operand1=11&operator=*&operand2=55 HTTP/1.1");
+        RequestLine requestLine = new RequestLine("GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1");
         assertThat(requestLine).isNotNull();
+        assertThat(requestLine).isEqualTo(new RequestLine("GET", "/calculate", "operand1=11&operator=*&operand2=55"));
     }
 }
