59e485e5a9632cd58cea9571d7f85d98944856d4
rugii913 <rugii913@gmail.com>
2023-09-03 00:49:04 +0900

    p1 계웹-key value String을 파라미터로 받는 QueryString

--- part1/mvcPractice/src/test/java/org/example/wasPractice/QueryStringTest.java
+++ part1/mvcPractice/src/test/java/org/example/wasPractice/QueryStringTest.java
@@ -1,5 +1,6 @@
 package org.example.wasPractice;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class QueryStringTest {
@@ -7,8 +8,7 @@ public class QueryStringTest {
     // operand1=11
     @Test
     void createTest() {
-        QueryString queryString = new QueryString("operand1=11");
-
-        assertThat(queryString).isNotNull();
+        QueryString queryString = new QueryString("operand1", "11");
+        Assertions.assertThat(queryString).isNotNull();
     }
 }
