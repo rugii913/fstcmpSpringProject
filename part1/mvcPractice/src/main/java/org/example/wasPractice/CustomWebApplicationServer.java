1a6d21ae90daa570a0ea0223c3b2682d0c34f698
rugii913 <rugii913@gmail.com>
2023-09-02 12:34:32 +0900

    p1 계웹-clientSocke에서 stream 얻고 감싸기, http 메시지 눈으로 확인
    ㅁCustomWebApplicationServer
     ㅇstart() 메서드에서
      - ClientSocket 인스턴스에서 InputStream, OutputStream 열기
      - InputStream -> InputStreamReader -> BufferedReader로 변환(line by line으로 읽기 위해)
      - OutputStream -> DataOutputStream으로 변환
    
     *들어온 http 메시지 System.out 찍어서 눈으로 확인해보기
      http요청이 실제로 들어오면 소켓이 accept()
      -> InputStream이 BufferedReader로 변환됨
      -> BufferedReader에서 readLine()으로 라인별로 읽고 System.out

--- part1/mvcPractice/src/main/java/org/example/wasPractice/CustomWebApplicationServer.java
+++ part1/mvcPractice/src/main/java/org/example/wasPractice/CustomWebApplicationServer.java
@@ -3,9 +3,10 @@ package org.example.wasPractice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
 
@@ -25,6 +26,28 @@ public class CustomWebApplicationServer {
 
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected!");
+
+                /**
+                 * Stpe1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
+                 */
+
+                // clientSocket 객체를 사용해서, InputStream, OutputStream 열기
+                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+                    // 하고 싶은 것: line by line으로 읽기
+                    // InputStream을 InputStreamReader로 바꿈(charset UTF8 형식으로) 
+                    // -> 내부에서 StreamDecoder의 static 메서드로 charset 정보 통해서 StreamDecoder 타입으로 갖고 있음
+                    // -> BufferedReader로 감싸서 line by line으로 읽을 준비하기
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    
+                    // OutputStream도 DataOutputStream으로 감싸줌
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    // http 메시지를 눈으로 확인하기 위한 부분
+                    String line;
+                    while ((line = br.readLine()) != "") {
+                        System.out.println(line);
+                    }
+                }
             }
         }
     }
