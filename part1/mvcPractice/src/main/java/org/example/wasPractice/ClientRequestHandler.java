9972e3a459e2c14c4f30d60874a3ebf52835cf95
rugii913 <rugii913@gmail.com>
2023-09-03 10:58:58 +0900

    p1 계웹-Step2 요청마다 스레드 생성 방식의 문제점

--- part1/mvcPractice/src/main/java/org/example/wasPractice/ClientRequestHandler.java
+++ part1/mvcPractice/src/main/java/org/example/wasPractice/ClientRequestHandler.java
@@ -18,9 +18,11 @@ public class ClientRequestHandler implements Runnable {
 
     /*
      * Step1- 사용자 요청을 메인 Thread가 처리하도록 한다. (완료)
-     *  - 다른 요청이 들어와도 처리 중인 요청이 끝날 때까지 기다려야하는 문제점
+     *  !!다른 요청이 들어와도 처리 중인 요청이 끝날 때까지 기다려야하는 문제점
      * Step2- 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
      *  - client의 요청별로 별도의 스레드 생성 -> 스레드 별로 InputStream, OutputStream 얻기 -> 아래 작업 수행 -> response를 클라이언트에 전달
+     *  !!thread는 생성될 때마다 독립적인 stack 메모리 공간을 할당받음 - 메모리 할당 작업은 비싼 작업 - 성능 문제
+     *  !!CPU 컨텍스트 스위치 문제, CPU나 메모리 사용량 증가, 서버 다운될 수 있음
      */
     @Override
     public void run() {
