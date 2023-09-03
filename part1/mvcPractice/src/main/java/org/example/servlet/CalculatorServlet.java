e36d4d7647f32ac5b7a31fa2e037a040eb9f7aca
rugii913 <rugii913@gmail.com>
2023-09-03 23:24:33 +0900

    p1 계서 Servlet 인터페이스 구현하여 계산기 서블릿 구현

--- part1/mvcPractice/src/main/java/org/example/servlet/CalculatorServlet.java
+++ part1/mvcPractice/src/main/java/org/example/servlet/CalculatorServlet.java
@@ -1,11 +1,14 @@
 package org.example.servlet;
 
+import org.example.oopPractice.calculator.domain.Calculator;
+import org.example.oopPractice.calculator.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/calculate")
 public class CalculatorServlet implements Servlet {
@@ -14,25 +17,36 @@ public class CalculatorServlet implements Servlet {
       Servlet - GenericServlet - HttpServlet 차례대로 구현
      */
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
+    private ServletConfig servletConfig;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
+        // Servlet 객체는 싱글톤으로 관리되므로 /calculate 요청이 여러 번 들어와도 init()은 한 번만 호출된다. Tomcat loadServlet() 참고
         log.info("init");
+        this.servletConfig = servletConfig;
     }
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         log.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
     }
 
     @Override
     public void destroy() {
-
+        // resource release
     }
 
     @Override
     public ServletConfig getServletConfig() {
-        return null;
+        return this.servletConfig;
     }
 
     @Override
