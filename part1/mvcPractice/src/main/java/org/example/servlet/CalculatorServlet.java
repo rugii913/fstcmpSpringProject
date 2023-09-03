278f0df5a1c476a42d8697bab7b975589e5abaa4
rugii913 <rugii913@gmail.com>
2023-09-03 17:51:38 +0900

    p1 계산기 서블릿 만들기-Servlet 인터페이스와 서블릿 컨테이너 설명

---
+++ part1/mvcPractice/src/main/java/org/example/servlet/CalculatorServlet.java
@@ -0,0 +1,8 @@
+package org.example.servlet;
+
+public class CalculatorServlet {
+    /*
+      Servlet 인터페이스: 서블릿 컨테이너(ex. 톰캣)와 상호작용하기 위한 메서드들(1.생명주기 관련 메서드들, 2.기타 메타정보 메서드들)
+      Servlet - GenericServlet - HttpServlet 차례대로 구현
+     */
+}
