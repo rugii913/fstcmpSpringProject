159f27611e15cdcc2392965885411e7325d1f939
rugii913 <rugii913@gmail.com>
2023-09-03 17:26:10 +0900

    p1 CGI.. Counter - state가 있는 형태

--- part1/mvcPractice/src/main/java/org/example/wasPractice/counter/Counter.java
+++ part1/mvcPractice/src/main/java/org/example/wasPractice/counter/Counter.java
@@ -1,9 +1,25 @@
 package org.example.wasPractice.counter;
 
 public class Counter implements Runnable {
-    
+    private int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public int getValue() {
+        return count;
+    }
+
     @Override
     public void run() {
-
+        this.increment();
+        System.out.println("Value for Thread After increment " + Thread.currentThread().getName() + " " + this.getValue());
+        this.decrement();
+        System.out.println("Value for Thread at last " + Thread.currentThread().getName() + " " + this.getValue());
     }
 }
