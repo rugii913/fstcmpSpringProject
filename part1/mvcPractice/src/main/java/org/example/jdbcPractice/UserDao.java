520aefc1598d75f704300da475dca2b9085803c4
rugii913 <rugii913@gmail.com>
2023-09-11 16:17:48 +0900

    p1 JDBC-UserDao 대충 작성 *테스트는 통과

--- part1/mvcPractice/src/main/java/org/example/jdbcPractice/UserDao.java
+++ part1/mvcPractice/src/main/java/org/example/jdbcPractice/UserDao.java
@@ -1,10 +1,89 @@
 package org.example.jdbcPractice;
 
+import java.sql.*;
+
 public class UserDao {
-    public void create(User user) {
+
+    private Connection getConnection() {
+        String url = "jdbc:h2:mem:localhost/~/mvcPractice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+        String id = "sa";
+        String pw = "";
+
+        try {
+            Class.forName("org.h2.Driver");
+            return DriverManager.getConnection(url, id, pw);
+        } catch (Exception ex) {
+            return null;
+        }
     }
 
-    public User findByUserId(String wizard) {
-        return null;
+    public void create(User user) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+
+            pstmt.executeUpdate();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } finally {
+                if (con != null) {
+                    con.close();
+                }
+            }
+        }
+    }
+
+    public User findByUserId(String userId) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS WHERE userId = ?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, userId);
+
+            rs = pstmt.executeQuery();
+
+            User user = null;
+            if (rs.next()) {
+                user = new User(
+                        rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email")
+                );
+            }
+
+            return user;
+        } finally {
+            try {
+                try {
+                    if (rs != null) {
+                        rs.close();
+                    }
+                } finally {
+                    if (pstmt != null) {
+                        pstmt.close();
+                    }
+                }
+            } finally {
+                if (con != null) {
+                    con.close();
+                }
+            }
+        }
     }
 }
--- part1/mvcPractice/src/test/java/org/example/jdbcPractice/UserDaoTest.java
+++ part1/mvcPractice/src/test/java/org/example/jdbcPractice/UserDaoTest.java
@@ -7,6 +7,8 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
+import java.sql.SQLException;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class UserDaoTest {
@@ -20,7 +22,7 @@ public class UserDaoTest {
     }
 
     @Test
-    void createTest() {
+    void createTest() throws SQLException {
         UserDao userDao = new UserDao();
 
         userDao.create(new User("wizard", "password", "name", "email"));
