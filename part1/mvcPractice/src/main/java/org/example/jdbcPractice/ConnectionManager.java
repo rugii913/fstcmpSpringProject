44e1a90da71c6ce395ad585cabeb348922195cd9
rugii913 <rugii913@gmail.com>
2023-09-11 16:41:28 +0900

    p1 JDBC-DataSource 및 Connection 획득 코드 정리 *
    *Connection 획득은 모두 ConnectionManager에서 처리하도록 코드를 모아놓음

--- part1/mvcPractice/src/main/java/org/example/jdbcPractice/ConnectionManager.java
+++ part1/mvcPractice/src/main/java/org/example/jdbcPractice/ConnectionManager.java
@@ -8,20 +8,38 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    public static Connection getConnection() throws SQLException {
-        return getDataSource().getConnection();
-    }
+    private static final String DB_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:mem:localhost/~/mvcPractice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    private static final String DB_USERNAME = "sa";
+    private static final String DB_PASSWORD = "";
+    private static final int MAX_POOL_SIZE = 10;
 
-    public static DataSource getDataSource() {
+    private static final DataSource ds ;
+
+    static { // DataSource는 하나여야 하므로 static 초기화 블럭으로 초기화(?)
         HikariDataSource hikariDataSource = new HikariDataSource(); // CP로 Hikari 사용할 것이므로 HikariDataSource 사용
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem:localhost/~/mvcPractice;MODE=MySQL;DB_CLOSE_DELAY=-1");
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
         // JdbcUrl 수정 https://www.h2database.com/html/features.html#embedded_databases - Database URL Overview 참고
         // cf. 이 형태는 connection은 embedded로, 저장은 in-memory 형태로 연결하는 것
         // https://kukim.tistory.com/105도 참고 // 구글 검색 키워드 h2 db embedded url
-        hikariDataSource.setUsername("sa");
-        hikariDataSource.setPassword("");
+        hikariDataSource.setUsername(DB_USERNAME);
+        hikariDataSource.setPassword(DB_PASSWORD);
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
 
-        return hikariDataSource;
+        ds = hikariDataSource;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public static DataSource getDataSource() {
+        return ds;
     }
 }
