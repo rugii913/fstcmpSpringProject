e43e4166a9e049ef8846ae8bf162e74ec232ec03
rugii913 <rugii913@gmail.com>
2023-09-11 15:59:23 +0900

    p1 JDBC-ConnectionManager DB URL 오류 상태 해결 *주석 참고

--- part1/mvcPractice/src/main/java/org/example/jdbcPractice/ConnectionManager.java
+++ part1/mvcPractice/src/main/java/org/example/jdbcPractice/ConnectionManager.java
@@ -9,7 +9,10 @@ public class ConnectionManager {
     public static DataSource getDataSource() {
         HikariDataSource hikariDataSource = new HikariDataSource(); // CP로 Hikari 사용할 것이므로 HikariDataSource 사용
         hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem//localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
+        hikariDataSource.setJdbcUrl("jdbc:h2:mem:localhost/~/mvcPractice;MODE=MySQL;DB_CLOSE_DELAY=-1");
+        // JdbcUrl 수정 https://www.h2database.com/html/features.html#embedded_databases - Database URL Overview 참고
+        // cf. 이 형태는 connection은 embedded로, 저장은 in-memory 형태로 연결하는 것
+        // https://kukim.tistory.com/105도 참고 // 구글 검색 키워드 h2 db embedded url
         hikariDataSource.setUsername("sa");
         hikariDataSource.setPassword("");
 
