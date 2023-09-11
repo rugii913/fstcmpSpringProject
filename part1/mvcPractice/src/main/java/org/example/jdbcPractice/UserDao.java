e559c1c71c08ecc3ca45c061da3fb2feae8505a9
rugii913 <rugii913@gmail.com>
2023-09-11 15:35:05 +0900

    p1 JDBC-User 클래스 작성-필드,getter, equals, hashCode *실패 상태
    *UserDao 메서드들 작성하지 않았으므로 실패하는 상태

---
+++ part1/mvcPractice/src/main/java/org/example/jdbcPractice/UserDao.java
@@ -0,0 +1,10 @@
+package org.example.jdbcPractice;
+
+public class UserDao {
+    public void create(User user) {
+    }
+
+    public User findByUserId(String wizard) {
+        return null;
+    }
+}
