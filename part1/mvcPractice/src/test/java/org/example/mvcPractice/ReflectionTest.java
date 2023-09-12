7d8e9e717451590850cf5aec8f255fdbc95bdc44
rugii913 <rugii913@gmail.com>
2023-09-12 17:04:03 +0900

    p1 MVC-리플렉션-클래스 정보들(필드, 생성자, 메서드) 출력 테스트

--- part1/mvcPractice/src/test/java/org/example/mvcPractice/ReflectionTest.java
+++ part1/mvcPractice/src/test/java/org/example/mvcPractice/ReflectionTest.java
@@ -3,15 +3,18 @@ package org.example.mvcPractice;
 import org.example.mvcPractice.annotation.Component;
 import org.example.mvcPractice.annotation.Controller;
 import org.example.mvcPractice.annotation.Service;
+import org.example.mvcPractice.model.User;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @Controller 어노테이션이 설정되어 있는 모든 클래스를 찾아서 출력한다.
@@ -33,5 +36,18 @@ public class ReflectionTest {
         beans.addAll(reflections.getTypesAnnotatedWith(annotationClazz));
 
         return beans;
+    }
+    
+    @Test // 클래스에 대한 모든 정보를 출력하는 테스트 메서드
+    void showClass() {
+        Class<User> clazz = User.class;
+        logger.debug(clazz.getName());
+
+        // 모든 필드 출력 - private까지 출력된다.
+        logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        // 모든 생성자 출력
+        logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+        // 모든 메서드 출력
+        logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
     }
 }
