8852e0d0e77008f5a992f08a78824171c86beb73
rugii913 <rugii913@gmail.com>
2023-09-12 12:11:21 +0900

    패키지 정리

--- part1/mvcPractice/src/main/java/org/example/mvcPractice/controller/HomeController.java
+++ part1/mvcPractice/src/main/java/org/example/mvcPractice/controller/HomeController.java
@@ -1,8 +1,8 @@
-package org.example.controller;
+package org.example.mvcPractice.controller;
 
-import org.example.annotation.Controller;
-import org.example.annotation.RequestMapping;
-import org.example.annotation.RequestMethod;
+import org.example.mvcPractice.annotation.Controller;
+import org.example.mvcPractice.annotation.RequestMapping;
+import org.example.mvcPractice.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
