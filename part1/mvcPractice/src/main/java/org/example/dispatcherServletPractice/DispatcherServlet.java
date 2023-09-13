43fba29b6bc993b336cbcc4ccbc8cd26c36d38c9
rugii913 <rugii913@gmail.com>
2023-09-13 16:15:21 +0900

    p1 MVC-찾은 viewName으로 forward/webapps 폴더에 jsp 뷰 작성 및 실행해보기
    ? 현재 문제점
    - jsp 파일 taglib 부분에 빨간줄
    - /favicon.ico 요청 때문에 exception 터짐

--- part1/mvcPractice/src/main/java/org/example/dispatcherServletPractice/DispatcherServlet.java
+++ part1/mvcPractice/src/main/java/org/example/dispatcherServletPractice/DispatcherServlet.java
@@ -4,6 +4,7 @@ import org.example.dispatcherServletPractice.controller.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,6 +25,7 @@ public class DispatcherServlet extends HttpServlet {
     public void init() throws ServletException { // DispatcherServlet이 인스턴스화될 때 호출
         // cf. 톰캣이 뜰 때 Servlet 타입 클래스를 싱글톤으로 인스턴스화하면서 그 때 init() 메서드 호출함
         // ***** 톰캣 코드 더 찾아보자 *****
+        requestMappingHandlerMapping = new RequestMappingHandlerMapping();
         requestMappingHandlerMapping.init(); // 포함하고 있는 RequestMappingHandlerMapping 객체를 초기화(맵 자료구조 초기화)
     }
 
@@ -31,9 +33,14 @@ public class DispatcherServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("[DispatcherServlet] service started");
 
-        Controller handler = requestMappingHandlerMapping.findHandler(request.getRequestURI()); // 매핑 작업 위임 -> uri에 맞는 컨트롤러(핸들러) 돌려받음
+        String uri = request.getRequestURI();
+        log.info("request uri= {}", uri);
+        Controller handler = requestMappingHandlerMapping.findHandler(uri); // 매핑 작업 위임 -> uri에 맞는 컨트롤러(핸들러) 돌려받음
         try {
             String viewName = handler.handleRequest(request, response); // 찾은 컨트롤러에 handleRequest 위임(뷰의 이름을 스트링으로 반환 받기)
+
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+            requestDispatcher.forward(request, response);
         } catch (Exception e) {
             log.error("exception occurred: [{}]", e.getMessage(), e);
             throw new RuntimeException(e);
---
+++ part1/mvcPractice/webapps/home.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset-UTF-8" pageEncoding="UTF-8" language="java" %>
+<%--pageEncoding="UTF-8" 부분 없으면 한글 깨져서 나옴--%>
+<%--cf. 현재 request에서 /favicon.ico까지 같이 날라와서 exception 터지는 상태--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--여기 빨간줄 뜨는 건 뭐가 문제인지는 모르겠음--%>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>home</title>
+</head>
+<body>
+페이지 Home
+</body>
+</html>
