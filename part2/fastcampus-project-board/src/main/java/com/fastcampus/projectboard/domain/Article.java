1d4ca9e9a8c5b8f09ec2deca927da585393177a0
rugii913 <rugii913@gmail.com>
2023-09-19 11:57:25 +0900

    p2 #10-도메인 코드를 엔티티로 변환
    ㅁ1개의 도메인 클래스 == 1개의 테이블 매칭되도록 설계함
     ㅇ생성자와 equals(), hashcode() 등이 도메인 맥락에 맞게끔 디테일하게 설정

--- part2/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ part2/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,16 +1,74 @@
 package com.fastcampus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title; // 제목
-    private String content; // 본문
-    private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt; // 생성일시
-    private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
-    private String modifiedBy; // 수정자
+    @Setter @Column(nullable = false) private String title; // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 본문
+
+    @Setter private String hashtag; // 해시태그
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return Objects.equals(id, article.id);
+        // return id != null && id.equals(article.id); 강의코드는 왼쪽과 같았으나, 자동으로 만들어준 위 코드를 사용해도 될 듯하다.
+        // cf. 강의코드에서 id != null을 붙인 것은 id가 부여되기 전, 즉 영속화시키기 전이라면 동등성 검사 자체를 의미 없는 것으로 보겠다는 것
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
