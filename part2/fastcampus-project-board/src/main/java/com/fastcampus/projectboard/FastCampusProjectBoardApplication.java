05740455008ec9ca8fc9b50ad1867d18a9a66cc6
rugii913 <rugii913@gmail.com>
2023-09-19 18:10:14 +0900

    p2 (참고사항) SpringApplication.run()-AutoCloseable 문제 주석
    ㅁSpringApplication.run()-AutoCloseable 관련 IntelliJ inspections 문제
     => IntelliJ 2022.1.2에서는 조치된 사항

--- part2/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
+++ part2/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -8,6 +8,11 @@ public class FastCampusProjectBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FastCampusProjectBoardApplication.class, args);
+        // cf. https://youtrack.jetbrains.com/issue/IDEA-288922
+        // IntelliJ 2021.3.2 ~ 2022.1.1 버전에서는 run에 오류 표시 뜨는 문제 있었음(AutoCloseable 관련)
+        // 강의에서는 설정 > Editor > Inspections에서
+        // Resource management > AutoCloseable used without 'try-with-resources' 관련 검사 문제
+        // => Ignore 'AutoCloseable' returned by this method로 이 메서드에 대해서는 AutoCloseable 검사 중단
     }
 
 }
