b614cec52ce8c7e7ea58a37d3e9a8b8806b505f9
rugii913 <rugii913@gmail.com>
2023-09-20 17:27:13 +0900

    p2 #16-로그인 컨트롤러 테스트+시큐리티 설정을 컨트롤러 테스트에 반영
    ㅁ스프링 시큐리티가 자동 생성한 `/login` 컨트롤러를 테스트
    ㅁ테스트에서 시큐리티 설정 사용 위해 @Import(SecurityConfig.class) 추가

---
+++ part2/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -0,0 +1,39 @@
+package com.fastcampus.projectboard.controller;
+
+import com.fastcampus.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("View 컨트롤러 - 인증 부분")
+@Import(SecurityConfig.class)
+@WebMvcTest
+public class AuthControllerTest {
+
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) { // 테스트에서는 생성자 하나라도 @Autowired 생략 불가능
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 로그인 페이지 - 정상 호출") // Spring security로 로그인 페이지 뷰 정상적으로 연결되는지
+    @Test
+    public void givenNothing_whenTryingToLogIn_thenReturnsLogInView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                // .andExpect(view().name("articles/index")) // 자동 생성이므로 view name 없음
+                // .andExpect(model().attributeExists("articles")); // attribute 안 봄
+    }
+}
