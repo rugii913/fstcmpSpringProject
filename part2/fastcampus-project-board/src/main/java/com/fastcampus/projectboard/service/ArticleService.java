6e436c5a90072a6d9b46869d27edfb74d526ffd0
rugii913 <rugii913@gmail.com>
2023-09-23 12:27:41 +0900

    p2 #21-비즈니스 로직 구현
    ㅁ테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료
     ㅇArticleService: 비즈니스 로직
     ㅇArticleRepository: 비즈니스 로직을 위해 필요한 JPA 쿼리메서드 추가
     ㅇArticleServiceTest
      - ArticleServiceTest 검증 findAll(~)의 누락된 pageable 인자 추가, content 오타 수정
     ㅇArticleDto
      - 누락된 메서드 toEntity() 추가

--- part2/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ part2/fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -1,15 +1,21 @@
 package com.fastcampus.projectboard.service;
 
+import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.type.SearchType;
 import com.fastcampus.projectboard.dto.ArticleDto;
 import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -19,21 +25,44 @@ public class ArticleService {
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        return Page.empty();
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+            // TODO: hashtag searchKeyword # 관련, 정책 및 리팩토링 필요
+        };
     }
 
     @Transactional(readOnly = true)
     public ArticleWithCommentsDto getArticle(long articleId) {
-        return null;
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentsDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
+        // findById(~) JPA에서 원래 갖고 있는 메서드
     }
 
     public void saveArticle(ArticleDto dto) {
-
+        articleRepository.save(dto.toEntity());
     }
 
     public void updateArticle(ArticleDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.id());
+            if (dto.title() != null) { article.setTitle(dto.title()); }
+            if (dto.content() != null) { article.setContent(dto.content()); }
+            article.setHashtag(dto.hashtag());
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
 }
