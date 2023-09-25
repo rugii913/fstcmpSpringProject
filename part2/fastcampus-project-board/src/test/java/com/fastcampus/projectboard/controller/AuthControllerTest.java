270e5aaad116d3b50bfb6ef1be82d15e3cf99a92
rugii913 <rugii913@gmail.com>
2023-09-25 13:09:11 +0900

    p2 #28-(부수적)예제 문자 삭제, 로그인 테스트 문제 해결
    ㅁ부트스트랩 예제 코드를 사용하면서 복사된 example 문자열 삭제
    ㅁ`/login` 페이지는 직접 만든 컨트롤러가 아닌 스프링 시큐리티가 해주는 작업이므로
     ㅇ컨트롤러 테스트에서 읽어야 할 컨트롤러 빈이 없음
     ㅇ이를 `Void.class`로 표현

--- part2/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ part2/fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("View 컨트롤러 - 인증 부분")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(Void.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -32,7 +33,8 @@ public class AuthControllerTest {
         // When & Then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andDo(MockMvcResultHandlers.print());
                 // .andExpect(view().name("articles/index")) // 자동 생성이므로 view name 없음
                 // .andExpect(model().attributeExists("articles")); // attribute 안 봄
     }
