9178ba066392b87fe845b6349e7c5956b796e8f6
rugii913 <rugii913@gmail.com>
2023-10-16 03:04:49 +0900

    p3-ch1-Multi Module-(스프링 빈)다른 모듈의 빈을 참조하는 더 나은 방법 - scanBasePackages 선언
    ㅁmain app의 @SpringBootApplication 어노테이션에 scanBasePackages 선언
     ㅇmoduleapi, modulecommon 패키지 포함시킴
      *모듈이 많아졌을 때에도 원하는 패키지만 명시적으로 지정 가능
    cf. @Import(~.class)로 원하는 일부 빈만 가져오는 것도 가능함

--- part3/multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ part3/multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -1,9 +1,12 @@
-package dev.be;
+package dev.be.moduleapi;
 
+import dev.be.modulecommon.service.CommonDemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"})
+// @Import(CommonDemoService.class)
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
