08f920a58a250a6f83cb6a9ac84df8f4941f0bd2
rugii913 <rugii913@gmail.com>
2023-10-16 15:04:45 +0900

    p6-ch1-MMDB-(*에러 해결)외부 모듈까지 JPA가 스캔하도록 main app에서 스캔 범위 지정
    ㅁmain app에서 @EntityScan, @EnableRepository 어노테이션에 스캔 패키지를 명시적으로 지정하여, JPA가 외부 모듈까지 스캔하도록 설정함

--- part3/multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ part3/multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -3,10 +3,15 @@ package dev.be.moduleapi;
 import dev.be.modulecommon.service.CommonDemoService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"})
 // @Import(CommonDemoService.class)
+@EntityScan("dev.be.modulecommon.domain")
+@EnableJpaRepositories(basePackages = "dev.be.modulecommon.repositories")
+// 간단하게 참고 - https://yousrain.tistory.com/37 - 외부 모듈의 클래스는 JPA의 스캔 대상이 아니므로, JPA 스캔 대상으로 명시
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
