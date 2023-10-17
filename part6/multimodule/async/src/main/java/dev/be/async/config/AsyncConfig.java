0850b45e8e389a86c3e85c7c43ef85bd529629b8
rugii913 <rugii913@gmail.com>
2023-10-17 15:33:06 +0900

    (별도)Async 프로젝트를 multimodule 프로젝트의 async 모듈로 만들기 시도-파일 옮기기 !빌드 실패 상태

---
+++ part6/multimodule/async/src/main/java/dev/be/async/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package dev.be.async.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync // async 동작할 수 있는 설정 끝
+public class AsyncConfig {
+}
