57b83486442a9005aa65b3d43af6346b907a1cb7
rugii913 <rugii913@gmail.com>
2023-10-17 15:40:23 +0900

    (별도)완전히 분리된 프로젝트였던 Async 프로젝트 및 폴더, 파일 삭제

--- part6/Async/src/main/java/dev/be/async/config/AsyncConfig.java
+++
@@ -1,9 +0,0 @@
-package dev.be.async.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-@Configuration
-@EnableAsync // async 동작할 수 있는 설정 끝
-public class AsyncConfig {
-}
